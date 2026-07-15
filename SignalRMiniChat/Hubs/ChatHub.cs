1f8f9ab75c838de5e81e81d60fb405d792fb9d5e
melisa <melisa@DESKTOP-LI5CD3F>
2026-07-16 01:32:05 +0300

    Add project files.

---
+++ SignalRMiniChat/Hubs/ChatHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRMiniChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+    }
+}
---
+++ SignalRMiniChat/Program.cs
@@ -0,0 +1,12 @@
+using SignalRMiniChat.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
