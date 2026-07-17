42f044d62471f7c81d49db80635d5797c2022203
melisa <melisa@DESKTOP-LI5CD3F>
2026-07-17 17:04:45 +0300

    A SignalR-based in-room mini-chat feature has been added.

--- SignalRMiniChat/Hubs/ChatHub.cs
+++ SignalRMiniChat/Hubs/ChatHub.cs
@@ -4,5 +4,46 @@ namespace SignalRMiniChat.Hubs
 {
     public class ChatHub : Hub
     {
+        public async Task JoinRoom(
+            string? oldRoomName,
+            string newRoomName,
+            string user)
+        {
+            if (!string.IsNullOrEmpty(oldRoomName))
+            {
+                await Groups.RemoveFromGroupAsync(
+                    Context.ConnectionId,
+                    oldRoomName);
+
+                await Clients
+                    .Group(oldRoomName)
+                    .SendAsync(
+                        "ReceiveSystemMessage",
+                        user + " " + oldRoomName + " odasından ayrıldı.");
+            }
+
+            await Groups.AddToGroupAsync(
+                Context.ConnectionId,
+                newRoomName);
+
+            await Clients
+                .Group(newRoomName)
+                .SendAsync(
+                    "ReceiveSystemMessage",
+                    user + " " + newRoomName + " odasına katıldı.");
+        }
+
+        public async Task SendMessage(
+            string roomName,
+            string user,
+            string message)
+        {
+            await Clients
+                .Group(roomName)
+                .SendAsync(
+                    "ReceiveMessage",
+                    user,
+                    message);
+        }
     }
 }
